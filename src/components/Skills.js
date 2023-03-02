e6ae4e047283733cc424d8fa382e095f6d9bb57d
oggunderscore <oggunderscore@gmail.com>
2023-03-02 14:31:24 -0800

    Implemented up-to-date changes for testing pulling information from canvas

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,51 +1,33 @@
-import { BadgeCheckIcon, ChipIcon } from "@heroicons/react/solid";
 import React from "react";
-import { skills } from "../data";
-const canvasAPI = require('node-canvas-api')
+import canvasAPI from "node-canvas-api";
+import { useState, useEffect } from "react";
 
-canvasAPI.getSelf()
-  .then(self => console.log(self))
+// Kevin's Class
 
-// This Class is for Kevin
+export default function About() {
+  const [courses, setCourses] = useState([]);
 
-export default function Skills() {
+  useEffect(() => {
+    async function fetchCourses() {
+      const self = await canvasAPI.getSelf();
+      const { id } = self;
+      const courses = await canvasAPI.getCoursesByUser(id);
+      setCourses(courses);
+      courses.map((course) => console.log("Course: " + course.name));
+    }
+    fetchCourses();
+
+  }, []);
   return (
-    <section id="skills">
-      {/* Tinker Below */}
-      {/* <div className="container px-5 py-10 mx-auto">
-        <div className="text-center mb-20">
-          <ChipIcon className="w-10 inline-block mb-4" />
-          <h1 className="sm:text-4xl text-3xl font-medium title-font text-white mb-4">
-            Skills &amp; Knowledge
-          </h1>
-          <h1 className="title-font 3sm:text-2xl text-3xl mb-4 font-medium text-white">
-            Efficient. Motivated. Strategic.
-          </h1>
-          <p className="mb-8 leading-relaxed">
-            Fast-learning computer science student ready to demonstrate persistent and appealing efforts with previous experience and skills towards future opportunities.
-          </p>
-          <div className="flex justify-center">
-            <a
-              href="#contact"
-              className="inline-flex text-white bg-green-200 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
-              Contact Me
-            </a>
-            <a
-              href="#projects"
-              className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
-              See My Past Work
-            </a>
-          </div>
-        </div>
-        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-          <img
-            className="object-cover object-center rounded"
-            alt="hero"
-            src="./coding.svg"
-          />
-        </div>
-      </div> */}
-      {/* Tinker Above */}
+    <section id="about">
+      <div>
+        <h2>Courses</h2>
+        <ul>
+          {courses.map((item) => (
+            <li key={item.id}>{item.name}</li>
+          ))}
+        </ul>
+      </div>
     </section>
   );
 }
