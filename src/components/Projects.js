f578ab5722b5e8d65be4d6c6c6b01587b7ce9a27
Citryte <citryte@outlook.com>
2023-03-04 17:14:27 -0800

    Added the ability to display a course

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,39 +6,32 @@ import { projects } from "../data";
 
 export default function Projects() {
   return (
-    <section id="projects" className="text-gray-400 bg-gray-900 body-font">
+    <section id="projects">
       {/* Tinker Below */}
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-            Hi, I'm not Kevin.
+            Courses:
             <br className="hidden lg:inline-block" />
           </h1>
           <h1 className="title-font 3sm:text-2xl text-3xl mb-4 font-medium text-white">
-            Efficient. Motivated. Strategic.
+            Course 1:
           </h1>
           <p className="mb-8 leading-relaxed">
-            Fast-learning computer science student ready to demonstrate persistent and appealing efforts with previous experience and skills towards future opportunities.
+            {projects[0].name} with {projects[0].prof}
           </p>
-          <div className="flex justify-center">
-            <a
-              href="#contact"
-              className="inline-flex text-white bg-green-200 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
-              Contact Me
-            </a>
-            <a
-              href="#projects"
-              className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
-              See My Past Work
-            </a>
-          </div>
-        </div>
-        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-          <img
-            className="object-cover object-center rounded"
-            alt="hero"
-            src="./coding.svg"
-          />
+          <h1 className="title-font 3sm:text-2xl text-3xl mb-4 font-medium text-white">
+            Due Dates:
+          </h1>
+          <p className="mb-8 leading-relaxed">
+            {projects[0].a_dates[0]}
+          </p>
+          <h1 className="title-font 3sm:text-2xl text-3xl mb-4 font-medium text-white">
+            Quiz Dates:
+          </h1>
+          <p className="mb-8 leading-relaxed">
+            {projects[0].q_dates[0]}
+          </p>
         </div>
       </div>
       {/* Tinker Above */}
