cbdc00a4e7e2d71cb59ae0ce5415d19f145b712d
Citryte <citryte@outlook.com>
2023-04-08 16:07:03 -0700

    Added rudimentary box

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,5 @@
-import { CodeIcon } from "@heroicons/react/solid";
 import React from "react";
 import { assignments, announcements, toDateStringBetter } from "../data";
-import {  } from "../data"
 
 // This Class is for Logan
 
@@ -21,7 +19,7 @@ export default function Projects() {
       {/* Tinker Below */}
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-          <div className="py-5">
+          <div className="py-5 bg-blue-300 border-2 border-grey-300 p-2">
             <h1 className="title-font 3sm:text-2xl text-3xl mb-4 font-medium text-white">
               Upcoming Due Dates
             </h1>
