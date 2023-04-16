bad6b8ae578aa1f901c7fec35609a37e1e7c9600
Citryte <citryte@outlook.com>
2023-04-15 17:06:35 -0700

    Removed box, commented out announcements

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -19,19 +19,17 @@ export default function Projects() {
       {/* Tinker Below */}
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-          <div className="py-5 bg-blue-300 border-2 border-grey-300 p-2">
-            <h1 className="title-font 3sm:text-2xl text-3xl mb-4 font-medium text-white">
-              Upcoming Due Dates
-            </h1>
-            <ol className="px-4" style={{listStyleType: 'disc'}}>
-              {
-                dueDates.map((item, index) =>
-                  <li key={index}>{item}</li>
-                )
-              }
-            </ol>
-          </div>
-          <div className="py-10">
+          <h1 className="title-font 3sm:text-2xl text-3xl mb-4 font-medium text-white">
+            Upcoming Due Dates
+          </h1>
+          <ol className="px-4" style={{listStyleType: 'disc'}}>
+            {
+              dueDates.map((item, index) =>
+                <li key={index}>{item}</li>
+              )
+            }
+          </ol>
+          {/* <div className="py-10">
             <h1 className="title-font 3sm:text-2xl text-3xl mb-4 font-medium text-white">
               Past 14 Days Announcements
             </h1>
@@ -42,7 +40,7 @@ export default function Projects() {
                 )
               }
             </ol>
-          </div>
+          </div> */}
         </div>
       </div>
       {/* Tinker Above */}
