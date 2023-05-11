eeade7973700c4956da73ed4216dac5f08d12631
Citryte <citryte@outlook.com>
2023-05-10 23:46:05 -0700

    Grouped assignments based on the current date

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { announcements, makeCanvasDateReadable, sortByDate } from "../data";
+import { announcements, makeCanvasDateReadable, sortByDate, dateToNum } from "../data";
 import { useAppData } from "../context/AppProvider";
 
 // This Class is for Logan
@@ -9,8 +9,15 @@ export default function Projects() {
   const { courses } = useAppData();
 
   let dueDates = [];
-  let annDates = [];
-  let dueDatesString = [];
+  let upcomingString = [];
+  let pastString = [];
+  let noDateString = [];
+
+  let today = 0;
+  let todayDate = new Date();
+  today = today + Number(todayDate.getDate()); // day
+  today = today + Number(todayDate.getMonth() + 1) * 100; // month
+  today = today + Number(todayDate.getFullYear()) * 10000; // year
 
   for(let i = 0; i < courses.length; i++){
     for(let j = 0; j < courses[i].assignments.length; j++)
@@ -26,40 +33,55 @@ export default function Projects() {
   dueDates = sortByDate(dueDates, true);
 
   for(let i = 0; i < dueDates.length; i++){
-      dueDatesString.push(dueDates[i].course + " - " + dueDates[i].assn_name + " - " + makeCanvasDateReadable(dueDates[i].assn_date));
+      if(dateToNum(dueDates[i].assn_date) >= today)
+        upcomingString.push(dueDates[i].course + " - " + dueDates[i].assn_name + " - " + makeCanvasDateReadable(dueDates[i].assn_date));
+      else if(dueDates[i].assn_date == "null")
+        noDateString.push(dueDates[i].course + " - " + dueDates[i].assn_name + " - " + makeCanvasDateReadable(dueDates[i].assn_date));
+      else
+        pastString.unshift(dueDates[i].course + " - " + dueDates[i].assn_name + " - " + makeCanvasDateReadable(dueDates[i].assn_date));
   }
-
-  // for(let j = 0; j < announcements.length; j++){
-  //   annDates.push(announcements[j].parent_course + " - " + announcements[j].name + " - " + makeCanvasDateReadable(announcements[j].post_date));
-  // }
 
   return (
     <section id="projects">
       {/* Tinker Below */}
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-          <h1 className="title-font 3sm:text-2xl text-3xl mb-4 font-medium text-white">
-            Upcoming Due Dates
-          </h1>
-          <ol className="px-4" style={{listStyleType: 'disc'}}>
-            {
-              dueDatesString.map((item, index) =>
-                <li key={index}>{item}</li>
-              )
-            }
-          </ol>
-          {/* <div className="py-10">
+          <div className="py-5">
             <h1 className="title-font 3sm:text-2xl text-3xl mb-4 font-medium text-white">
-              Past 14 Days Announcements
+              Upcoming Assignments
             </h1>
             <ol className="px-4" style={{listStyleType: 'disc'}}>
               {
-                annDates.map((item, index) =>
+                upcomingString.map((item, index) =>
                   <li key={index}>{item}</li>
                 )
               }
             </ol>
-          </div> */}
+          </div>
+          <div className="py-5">
+            <h1 className="title-font 3sm:text-2xl text-3xl mb-4 font-medium text-white">
+              Undated Assignments
+            </h1>
+            <ol className="px-4" style={{listStyleType: 'disc'}}>
+              {
+                noDateString.map((item, index) =>
+                  <li key={index}>{item}</li>
+                )
+              }
+            </ol>
+          </div>
+          <div className="py-5">
+            <h1 className="title-font 3sm:text-2xl text-3xl mb-4 font-medium text-white">
+              Past Assignments
+            </h1>
+            <ol className="px-4" style={{listStyleType: 'disc'}}>
+              {
+                pastString.map((item, index) =>
+                  <li key={index}>{item}</li>
+                )
+              }
+            </ol>
+          </div>
         </div>
       </div>
       {/* Tinker Above */}
